34a4916cdcf52ca72f7e9feb093dbbd2a7f03dc5
Kuumoneko <maingocnhat2007@gmail.com>
2025-09-15 21:48:37 +0700

    show today schedule by current subject and next subjects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import UI from "@/components/UI";
-import { useEffect,  useState } from "react";
+import { useEffect, useState } from "react";
 import Table from "../components/table/index";
 import Logout from "@/utils/logout";
 import full_schedule from "@/utils/schdule";
@@ -43,6 +43,9 @@ function create_day_schedule(date: string, schedule: SubjectInfo[]) {
 
 export default function Home() {
     const [today_sche, set_today_sche] = useState<SubjectInfo[]>([]);
+    const [curr_sub, set_curr_sub] = useState<SubjectInfo | null>(null);
+    const [next_sub, set_next_sub] = useState<SubjectInfo[] | null>(null);
+
     const [closestDay, setClosestDay] = useState<string | null>(null);
     const [closestDaySche, setClosestDaySche] = useState<SubjectInfo[]>([]);
 
@@ -75,6 +78,59 @@ export default function Home() {
 
                 const today_subject = create_day_schedule(today, schedule);
                 set_today_sche(today_subject);
+
+                if (today_subject.length > 0) {
+                    const now = new Date();
+
+                    const current_subjects = today_subject.filter(
+                        (item: SubjectInfo) => {
+                            const { startTime, endTime } = item;
+
+                            const StartHour = Number(startTime.split(":")[0]);
+                            const StartMin = Number(startTime.split(":")[1]);
+                            const EndHour = Number(endTime.split(":")[0]);
+                            const EndMin = Number(endTime.split(":")[1]);
+
+                            const temping = new Date();
+
+                            const start = temping.setHours(
+                                StartHour,
+                                StartMin,
+                                0,
+                                0
+                            );
+
+                            const end = temping.setHours(EndHour, EndMin, 0, 0);
+
+                            return (
+                                start <= now.getTime() && now.getTime() <= end
+                            );
+                        }
+                    );
+
+                    const next_subjects = today_subject.filter(
+                        (item: SubjectInfo) => {
+                            const { startTime } = item;
+
+                            const StartHour = Number(startTime.split(":")[0]);
+                            const StartMin = Number(startTime.split(":")[1]);
+
+                            const temping = new Date();
+
+                            const start = temping.setHours(
+                                StartHour,
+                                StartMin,
+                                0,
+                                0
+                            );
+
+                            return now.getTime() <= start;
+                        }
+                    );
+
+                    set_curr_sub(current_subjects[0] ?? null);
+                    set_next_sub(next_subjects ?? null);
+                }
 
                 const allDates: any[] = [
                     ...new Set(schedule.flatMap((s: any) => s.dates)),
@@ -111,14 +167,31 @@ export default function Home() {
                     mode === "row" && "ml-10"
                 } w-[100%] max-x-[1500px]`}
             >
-                <div className="flex flex-col items-center justify-center w-[100%]">
-                    <h1>Hôm nay là {parseDaytoVietnamese(today)}</h1>
-                    {today_sche.length > 0 ? (
-                        <Table subjects={today_sche} />
-                    ) : (
-                        <p className="mt-4">Hôm nay không có môn học.</p>
-                    )}
-                </div>
+                <h1>Hôm nay là {parseDaytoVietnamese(today)}</h1>
+                {today_sche.length > 0 ? (
+                    <div className="flex flex-col items-center justify-center w-[100%]">
+                        {curr_sub && (
+                            <div className="flex flex-col items-center justify-center w-[100%] mt-8">
+                                <h1>Tiết hiện tại</h1>
+                                <Table subjects={[curr_sub]} />
+                            </div>
+                        )}
+                        {next_sub && next_sub.length > 0 && (
+                            <div className="flex flex-col items-center justify-center w-[100%] mt-8">
+                                <h1>Các tiết tiếp theo</h1>
+                                <Table subjects={next_sub} />{" "}
+                            </div>
+                        )}
+                        {!curr_sub && (!next_sub || next_sub.length === 0) && (
+                            <div>
+                                Hôm nay đã hết tiết học rồi, nghỉ ngơi đi bạn
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <p className="mt-4">Hôm nay không có môn học.</p>
+                )}
+
                 {closestDay && closestDaySche.length > 0 && (
                     <div className="flex flex-col items-center justify-center w-[100%] mt-8">
                         <h1>
